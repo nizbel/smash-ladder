c318c6655972ab9f322e41e6a4e47e53e43392b3
nizbel <kingbowserii@gmail.com>
2019-04-20 01:39:59 -0300

    Mask funcionando para data/hora

--- smashLadder/utils.py
+++ smashLadder/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Funções para uso geral"""
-from django.forms.fields import BooleanField
+from django.forms.fields import BooleanField, DateTimeField
 
 
 def preparar_classes_form(form):
@@ -9,6 +9,10 @@ def preparar_classes_form(form):
         if isinstance(field, BooleanField):
             field.widget.attrs.update({
                     'class': 'form-control-checkbox'
+            })
+        elif isinstance(field, DateTimeField):
+            field.widget.attrs.update({
+                    'class': 'form-control data-hora'
             })
         else:
             field.widget.attrs.update({
