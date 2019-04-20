fb5efa8b9a46d98c4c7cc59796461de95c21d71e
nizbel <kingbowserii@gmail.com>
2019-04-20 03:16:44 -0300

    Adicionando telas para alterar email

--- smashLadder/management/commands/gerar_historico_ladder.py
+++ smashLadder/management/commands/gerar_historico_ladder.py
@@ -31,17 +31,5 @@ def copiar_info_atual_para_mes_passado():
                                                    mes=mes, ano=ano)
                 historico_ladder.save()
                 
-                
-            # TODO apagar isso aqui
-            for _ in range(13):
-                mes -= 1
-                if mes == 0:
-                    mes = 12
-                    ano -= 1
-                for posicao_ladder in PosicaoLadder.objects.all().order_by('posicao'):
-                    historico_ladder = HistoricoLadder(posicao=posicao_ladder.posicao, jogador=posicao_ladder.jogador,
-                                                       mes=mes, ano=ano)
-                    historico_ladder.save()
-                    
     except:
         raise
