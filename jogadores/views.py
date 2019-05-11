afc0a1e7a2240d4ad29def634dd9f73dd7784dc6
nizbel <kingbowserii@gmail.com>
2019-05-11 13:04:19 -0300

    Melhorando detalhamento de jogador

--- jogadores/views.py
+++ jogadores/views.py
@@ -39,9 +39,9 @@ def detalhar_jogador(request, username):
     qtd_desafios = 0
     for desafio in DesafioLadder.objects.filter(Q(desafiante=jogador) | Q(desafiado=jogador), cancelamentodesafioladder__isnull=True) \
             .order_by('data_hora'):
-        if desafio.desafiante == jogador and desafio.score_desafiante > desafio.score_desafiado:
+        if desafio.desafiante_id == jogador.id and desafio.score_desafiante > desafio.score_desafiado:
             qtd_vitorias += 1
-        elif desafio.desafiado == jogador and desafio.score_desafiante < desafio.score_desafiado:
+        elif desafio.desafiado_id == jogador.id and desafio.score_desafiante < desafio.score_desafiado:
             qtd_vitorias += 1
         qtd_desafios += 1
         percentual_vitorias = qtd_vitorias * 100 / qtd_desafios 
