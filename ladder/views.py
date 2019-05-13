93bf1943ceb3e61669700499d94e605f269c5aa4
nizbel <kingbowserii@gmail.com>
2019-05-13 01:46:47 -0300

    Adicionando posições a edição de desafio

--- ladder/views.py
+++ ladder/views.py
@@ -399,6 +399,11 @@ def editar_desafio_ladder(request, desafio_id):
                 try:
                     with transaction.atomic():
                         desafio_ladder = form_desafio_ladder.save(commit=False)
+                        
+                        # Adicionar posições
+                        desafio_ladder.posicao_desafiante = desafio_ladder.desafiante.posicao_em(desafio_ladder.data_hora)
+                        desafio_ladder.posicao_desafiado = desafio_ladder.desafiado.posicao_em(desafio_ladder.data_hora)
+                        
                         desafio_ladder.save()
                         
                         # Validar lutas
