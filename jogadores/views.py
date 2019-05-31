59610a1fa7c69af0ebd0fd71cfc2335cbb34e9f2
Guilherme de Deus <kingbowserii@gmail.com>
2019-05-31 23:52:23 +0000

    Testar gráfico

--- jogadores/views.py
+++ jogadores/views.py
@@ -91,13 +91,29 @@ def detalhar_jogador(request, username):
             # Preencher gráfico com variações a partir dessa data
             posicao_atual = posicao_inicial
             resultados_desafios = ResultadoDesafioLadder.objects.filter(jogador=jogador).annotate(data_hora=F('desafio_ladder__data_hora')) \
-                    .values('data_hora').annotate(alteracao_total=Sum('alteracao_posicao')) \
-                    .values('alteracao_total', 'data_hora').order_by('data_hora', 'desafio_ladder__posicao_desafiado')
+                    .values('data_hora').annotate(alteracao_total=Sum('alteracao_posicao')).annotate(tipo=Value(1)) \
+                    .values('alteracao_total', 'data_hora', 'tipo').order_by('data_hora', 'desafio_ladder__posicao_desafiado')
             resultados_decaimentos = ResultadoDecaimentoJogador.objects.filter(jogador=jogador).annotate(data_hora=F('decaimento__data')) \
-                    .values('data_hora').annotate(alteracao_total=Sum('alteracao_posicao')) \
-                    .values('alteracao_total', 'data_hora').order_by('data_hora', 'decaimento__posicao_inicial')
-            resultados_remocoes = Remocao.objects.filter(data__gt=data_inicial).annotate(data_hora=F('data')).order_by('data', 'posicao_jogador')
+                    .values('data_hora').annotate(alteracao_total=Sum('alteracao_posicao')).annotate(tipo=Value(2)) \
+                    .values('alteracao_total', 'data_hora', 'tipo').order_by('data_hora', 'decaimento__posicao_inicial')
+            resultados_remocoes = Remocao.objects.filter(data__gt=data_inicial).annotate(data_hora=F('data')).annotate(tipo=Value(3)) \
+                    .values('posicao_jogador', 'data_hora', 'tipo').order_by('data_hora', 'posicao_jogador')
             
+            resultados = resultados_desafios.extend(resultados_decaimentos).extend(resultados_remocoes)
+            
+            resultados.sort(key=lambda x: x.data_hora)
+            
+            for indice, resultado in enumerate(resultados):
+                if resultado.tipo in [1, 2]:
+                    posicao_atual += resultado['alteracao_total']
+                elif resultado.tipo == 3:
+                    if resultado['posicao_jogador'] < posicao_atual:
+                        posicao_atual -= 1
+                        
+                if indice == len(resultados)-1 or (resultado['data_hora'] != resultados[indice+1]['data_hora']):
+                    jogador.grafico_variacao_posicao.append({'x': resultado['data_hora'].strftime('%d/%m/%Y %H:%M'), 'y': posicao_atual})
+                        
+            # TODO remover
             for resultado in ResultadoDesafioLadder.objects.filter(jogador=jogador).annotate(data_hora=F('desafio_ladder__data_hora')) \
                     .values('data_hora').annotate(alteracao_total=Sum('alteracao_posicao')) \
                     .values('alteracao_total', 'data_hora').order_by('data_hora', 'desafio_ladder__posicao_desafiado'):
