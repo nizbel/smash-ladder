1aef562578fbeb66f93ec9e6bac84289e06e5aaf
nizbel <kingbowserii@gmail.com>
2019-06-01 03:48:38 -0300

    Corrigindo testes para utilizar stages validas

--- ladder/tests/test_views/test_add_registro_ladder.py
+++ ladder/tests/test_views/test_add_registro_ladder.py
@@ -7,7 +7,7 @@ from django.test.testcases import TestCase
 from django.urls.base import reverse
 from django.utils import timezone
 
-from jogadores.models import Jogador, Personagem
+from jogadores.models import Jogador, Personagem, StageValidaLadder
 from jogadores.tests.utils_teste import criar_jogadores_teste, SENHA_TESTE, \
     criar_stage_teste, criar_personagens_teste, criar_jogador_teste
 from ladder.models import DesafioLadder, PosicaoLadder, HistoricoLadder, Luta, \
@@ -36,6 +36,8 @@ class ViewAddDesafioLadderTestCase(TestCase):
         
         # Stage
         cls.stage = criar_stage_teste()
+        # Configurar stage como válida
+        StageValidaLadder.objects.create(stage=cls.stage)
         
         # Personagens
         criar_personagens_teste()   
