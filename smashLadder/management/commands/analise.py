0ee808ce278b12dc834da743e92759485b2c72bf
nizbel <kingbowserii@gmail.com>
2019-07-04 01:16:07 -0300

    Análises iniciais com pandas funcionando

--- smashLadder/management/commands/analise.py
+++ smashLadder/management/commands/analise.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from django.core.management.base import BaseCommand
 from django.db.models.expressions import F
 
 from ladder.models import DesafioLadder
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
 
 
 class Command(BaseCommand):
@@ -21,43 +22,58 @@ def analisar():
                                     .annotate(nick_desafiado=F('desafiado__nick')).values(
                                         'data_hora', 'nick_desafiante', 'score_desafiante', 'posicao_desafiante', 'nick_desafiado', 
                                         'score_desafiado', 'posicao_desafiado', 'desafio_coringa').order_by('data_hora')))
-#     print(desafios_df.head())
-    print(desafios_df['score_desafiante'].value_counts())
-    print(desafios_df['score_desafiado'].value_counts())
     
     # RESULTADO POR PAR DESAFIANTE/DESAFIADO
-#     resultado_pares_df = desafios_df.copy(True)
-#     
-#     resultado_pares_df['qtd_desafios'] = 1
-#     
-#     resultado_pares_df = resultado_pares_df.groupby(['nick_desafiante', 'nick_desafiado']).sum()
-#     resultado_pares_df['resultado'] = (resultado_pares_df['score_desafiante'] - resultado_pares_df['score_desafiado']) / resultado_pares_df['qtd_desafios']
-#     
-#     resultado_pares_df = resultado_pares_df.drop(['score_desafiante', 'posicao_desafiante', 'qtd_desafios',
-#                                         'score_desafiado', 'posicao_desafiado', 'desafio_coringa'], axis=1)
-#     
-#     resultado_pares_df = resultado_pares_df.reset_index(1)
-#     
-#     resultado_pares_df = resultado_pares_df.pivot(columns='nick_desafiado', values='resultado')
-#         
-#     print(resultado_pares_df)
-#     
-#     plt.rcParams.update({'font.size': 12, 'figure.figsize': (12, 8)})
-#     
-#     
-# #     Index= ['aaa', 'bbb', 'ccc', 'ddd', 'eee']
-# #     Cols = ['A', 'B', 'C', 'D']
-# #     df = pd.DataFrame(abs(np.random.randn(5, 4)), index=Index, columns=Cols)
-#     
-#     plt.pcolor(resultado_pares_df)
-#     plt.yticks(np.arange(0.5, len(resultado_pares_df.index), 1), resultado_pares_df.index)
-#     plt.xticks(np.arange(0.5, len(resultado_pares_df.columns), 1), resultado_pares_df.columns)
-# #     max_qtd_desafios = int(resultado_pares_df['qtd_desafios'].max())
-# #     resultado_pares_df.plot.scatter(x='nick_desafiante', y='nick_desafiado', c='resultado', cmap='viridis', 
-# #                                        s=250)
-#     
-#     
-#     plt.savefig('smashLadder/static/teste3.png')
+    resultado_pares_df = desafios_df.copy(True)
+     
+    # Adicionar coluna para contar qtd de desafios entre pares
+    resultado_pares_df['qtd_desafios'] = 1
+    
+    # Organizar pares de forma que o desafiante seja sempre o menor nome
+    resultado_pares_df['min'] = resultado_pares_df[['nick_desafiante', 'nick_desafiado']].min(axis=1)
+    
+    inverter_idx = resultado_pares_df['nick_desafiante'] != resultado_pares_df['min']
+    resultado_pares_df.loc[inverter_idx, ['nick_desafiante', 'nick_desafiado', 'score_desafiante', 'score_desafiado']] = resultado_pares_df.loc[inverter_idx, [
+        'nick_desafiado', 'nick_desafiante', 'score_desafiado', 'score_desafiante']].values
+    
+    # Agrupar registros de desafio entre mesmo par
+    resultado_pares_df = resultado_pares_df.groupby(['nick_desafiante', 'nick_desafiado']).sum()
+    
+    # Calcular resultado de partidas entre mesmo par
+    resultado_pares_df['resultado'] = (resultado_pares_df['score_desafiante'] - resultado_pares_df['score_desafiado']) / resultado_pares_df['qtd_desafios']
+    
+    # Remover colunas não utilizadas
+    resultado_pares_df = resultado_pares_df.drop(['score_desafiante', 'posicao_desafiante', 'qtd_desafios',
+                                        'score_desafiado', 'posicao_desafiado', 'desafio_coringa'], axis=1)
+    
+    # Remover índices
+    resultado_pares_df = resultado_pares_df.reset_index([0,1])
+    
+    # Adicionar pares alternos
+    aux_df = resultado_pares_df.copy(True)
+    aux_df[['nick_desafiante', 'nick_desafiado']] = aux_df[['nick_desafiado', 'nick_desafiante']].values
+    aux_df['resultado'] *= -1
+    
+    resultado_pares_df = resultado_pares_df.append(aux_df, sort=True, ignore_index=True)
+    
+    del(aux_df)
+    
+    resultado_pares_df = resultado_pares_df.pivot(index='nick_desafiante', columns='nick_desafiado', values='resultado')
+     
+    plt.rcParams.update({'font.size': 12, 'figure.figsize': (12, 14)})
+    plt.figure()
+     
+    plt.pcolor(resultado_pares_df)
+    plt.yticks(np.arange(0.5, len(resultado_pares_df.index), 1), resultado_pares_df.index)
+    plt.xticks(np.arange(0.5, len(resultado_pares_df.columns), 1), resultado_pares_df.columns, rotation='vertical')
+    plt.xlabel('Oponente enfrentado')
+    plt.title('Média de jogos ganhos ao enfrentar oponente')
+
+    color_bar = plt.colorbar(orientation='horizontal', label='Média de jogos ganhos')          
+    cbytick_obj = plt.getp(color_bar.ax.axes, 'yticklabels')               
+    plt.setp(cbytick_obj)
+    
+    salvar_imagem('acumulado_entre_jogadores.png', plt)
     
     # RESULTADO POR POSIÇÃO
     resultados_posicao_df = desafios_df.copy(True)
@@ -80,7 +96,6 @@ def analisar():
     perc_resultados_dif_pos_df = perc_resultados_dif_pos_df.drop(['data_hora', 'nick_desafiante', 'score_desafiante', 'posicao_desafiante', 'nick_desafiado', 
                                         'score_desafiado', 'posicao_desafiado', 'desafio_coringa'], axis=1)
     
-    print(perc_resultados_dif_pos_df)
     
     # ANÁLISE ESPECÍFICA
 #     nick_especifico = 'Mad'
@@ -93,20 +108,23 @@ def analisar():
     
 #     desafios_especifico.plot(kind='bar', x='oponente', y='score', title='Resultado desafiante')
     plt.rcParams.update({'font.size': 12, 'figure.figsize': (12, 8)})
+    plt.figure()
+    
     max_qtd_desafios = int(resultados_posicao_df['qtd_desafios'].max())
     resultados_posicao_df.plot.scatter(x='posicao_desafiante', y='posicao_desafiado', c='resultado', cmap='viridis', 
                                        s=resultados_posicao_df['qtd_desafios'] / max_qtd_desafios * 100)
     
-    plt.savefig('smashLadder/static/qtd_result_por_pos.png')
+    salvar_imagem('qtd_result_por_pos.png', plt)
 
     plt.rcParams.update({'font.size': 12, 'figure.figsize': (8, 5)})
+    plt.figure()
     
     val_min = int(perc_resultados_dif_pos_df['diferenca_posicoes'].describe()['min'])
     val_max = int(perc_resultados_dif_pos_df['diferenca_posicoes'].describe()['max'])
     variacao = val_max - val_min
     perc_resultados_dif_pos_df.hist(alpha=0.5, bins=variacao, by='resultado')
 
-    plt.savefig('smashLadder/static/vit_derr.png')
+    salvar_imagem('vit_derr.png', plt)
     
     perc_resultados_dif_pos_df['qtd_desafios'] = 1
 
@@ -123,7 +141,13 @@ def analisar():
     
     perc_resultados_dif_pos_df = perc_resultados_dif_pos_df.drop(['Derrota', 'Vitória'], axis=1)
     
-    
     perc_resultados_dif_pos_df.plot.bar(stacked=True)
-    plt.savefig('smashLadder/static/percentual.png')
+
+    salvar_imagem('percentual.png', plt)
+    
+def salvar_imagem(nome_arquivo, plot_ctrl):
+    caminho = 'smashLadder/static/analises/' + nome_arquivo
+    if os.path.isfile(caminho):
+        os.remove(caminho)
+    plot_ctrl.savefig(caminho)
     
