2b23605e359611cdefbb15a6b0268b577153415d
Guilherme de Deus <kingbowserii@gmail.com>
2019-07-30 22:39:05 +0000

    Preparando analise de resultados acumulados pra um jogador

--- smashLadder/views.py
+++ smashLadder/views.py
@@ -30,7 +30,7 @@ def home(request):
                                                   'top_10_ladder': top_10_ladder})
 
 def analises(request):
-    """Mostrar anÃ¡lises dos dados de desafios"""
+    """Mostrar análises dos dados de desafios"""
     imagens = analisar()
     for imagem in imagens:
         imagens[imagem] = 'analises/' + imagens[imagem]
@@ -56,16 +56,52 @@ def analise_resultado_acumulado_jogadores(request):
             jogadores_validos = PosicaoLadder.objects.all().values_list('jogador')
         else:
             jogadores_validos = HistoricoLadder.objects.filter(mes=mes, ano=ano).values_list('jogador')
-            
         
         desafios_df = pd.DataFrame(list(DesafioLadder.validados.filter(data_hora__lt=timezone.datetime(prox_ano, prox_mes, 1, 0, 0, tzinfo=timezone.get_current_timezone())) \
                                         .filter(desafiante__in=jogadores_validos, desafiado__in=jogadores_validos)
                                         .annotate(nick_desafiante=F('desafiante__nick')).annotate(nick_desafiado=F('desafiado__nick')).values(
-                                            'data_hora', 'nick_desafiante', 'score_desafiante', 'posicao_desafiante', 'nick_desafiado', 
-                                                    'score_desafiado', 'posicao_desafiado', 'desafio_coringa').order_by('data_hora')))
+                                            'data_hora', 'nick_desafiante', 'score_desafiante', 'nick_desafiado', 
+                                                    'score_desafiado').order_by('data_hora')))
         
+        desafios_df = analisar_resultado_acumulado_entre_jogadores(desafios_df, (mes, ano))
         
-                
+        # Trocar NaNs por None, para ser codificado em JSON
+        desafios_df = desafios_df.where(pd.notnull(desafios_df), None)
+        
+        return JsonResponse({'resultado_desafios': desafios_df.values.tolist(), 'jogador_enfrentado': desafios_df.columns.tolist(), 
+                             'jogador': desafios_df.index.tolist()})
+                             
+def analise_resultado_acumulado_para_um_jogador(request):
+    """Retorna dados sobre acumulado de resultados de desafios de um jogador"""
+    if request.is_ajax():
+        ano = int(request.GET.get('ano'))
+        mes = int(request.GET.get('mes'))
+        jogador_id = int(request.GET.get('jogador_id')
+        
+        # Verificar se jogador existe
+        jogador = get_object_or_404(Jogador, id=jogador_id)
+        
+        data_atual = timezone.localdate()
+        if ano > data_atual.year or (ano == data_atual.year and mes > data_atual.month):
+            ano = data_atual.year
+            mes = data_atual.month
+        
+        # Definir mes/ano final para resultados de desafios
+        (prox_mes, prox_ano) = mes_ano_prox(mes, ano)
+        
+        # Filtrar jogadores validos para mes/ano
+        if (ano, mes) == (data_atual.year, data_atual.month):
+            jogadores_validos = PosicaoLadder.objects.all().values_list('jogador')
+        else:
+            jogadores_validos = HistoricoLadder.objects.filter(mes=mes, ano=ano).values_list('jogador')
+        
+        desafios_df = pd.DataFrame(list(DesafioLadder.validados.filter(data_hora__lt=timezone.datetime(prox_ano, prox_mes, 1, 0, 0, tzinfo=timezone.get_current_timezone())) \
+                                        .filter(desafiante__in=jogadores_validos, desafiado__in=jogadores_validos)
+                                        .filter(Q(desafiante__id=jogador_id) | Q(desafiado__id=jogador_id))
+                                        .annotate(nick_desafiante=F('desafiante__nick')).annotate(nick_desafiado=F('desafiado__nick')).values(
+                                            'data_hora', 'nick_desafiante', 'score_desafiante', 'nick_desafiado', 
+                                                    'score_desafiado').order_by('data_hora')))
+        
         desafios_df = analisar_resultado_acumulado_entre_jogadores(desafios_df, (mes, ano))
         
         # Trocar NaNs por None, para ser codificado em JSON
