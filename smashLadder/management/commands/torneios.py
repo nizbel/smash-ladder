b24a50f81eb7b56f62411b63c1c9cf68b36d6db2
nizbel <kingbowserii@gmail.com>
2019-08-22 14:01:14 -0300

    Adicionando busca de torneios do Challonge

--- smashLadder/management/commands/torneios.py
+++ smashLadder/management/commands/torneios.py
@@ -1,38 +1,92 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
+from django.db import transaction
+from django.utils import timezone
 
 import challonge
 from conf.conf import CHALLONGE_USER, CHALLONGE_API_KEY
+from jogadores.models import Jogador
+from torneios.models import Torneio, JogadorTorneio, VitoriaAusencia, Partida, \
+    Time, Round
 
 
 class Command(BaseCommand):
     help = 'Teste challonge'
 
     def handle(self, *args, **options):
+        try:
+            with transaction.atomic():
+                # Tell pychallonge about your [CHALLONGE! API credentials](http://api.challonge.com/v1).
+                challonge.set_credentials(CHALLONGE_USER, CHALLONGE_API_KEY)
+                
+                # Retrieve a tournament by its id (or its url).
+                tournament = challonge.tournaments.show('atevalhalla2')
+                
+                # Torneio
+                torneio = Torneio(nome=tournament['name'], data=timezone.localtime(tournament['started-at']).date(),
+                                  url=tournament['full-challonge-url'], adicionado_por=Jogador.objects.get(nick='Niz'))
+                
+                torneio.save()
+                
+                # Retrieve the participants for a given tournament.
+                participants = challonge.participants.index(tournament['id'])
+                for participant in participants:
+                    # Verificar time
+                    delimitador_time = '|'
+                    if ' | ' in participant['name']:
+                        nome_time = participant['name'].split(delimitador_time)[0].strip()
+                        nome = participant['name'].split(delimitador_time)[1].strip()
+                        if Time.objects.filter(nome=nome_time).exists():
+                            time = Time.objects.get(nome=nome_time)
+                        else:
+                            time = Time(nome=nome_time)
+                            time.save()
+                    else:
+                        time = None
+                        nome = participant['name'].strip()
+                    jogador = JogadorTorneio(nome=nome, posicao_final=participant['final-rank'], torneio=torneio,
+                                             time=time, id_no_torneio=participant['id'])
+                    jogador.save()
+        
+                matches = challonge.matches.index(tournament['id'])
+                for match in matches:
 
-        # Tell pychallonge about your [CHALLONGE! API credentials](http://api.challonge.com/v1).
-        challonge.set_credentials(CHALLONGE_USER, CHALLONGE_API_KEY)
+                    # Verificar se round já está registrado
+                    if Round.objects.filter(torneio=torneio, indice=match['round']).exists():
+                        round_torneio = Round.objects.get(torneio=torneio, indice=match['round'])
+                    else:
+                        # TODO Gerar nome de torneio
+                        round_torneio = Round(torneio=torneio, indice=match['round'])
+                        round_torneio.save()
+                    
+                    ganhador = JogadorTorneio.objects.get(id_no_torneio=match['winner-id'])
+                    jogador_1 = JogadorTorneio.objects.get(id_no_torneio=match['player1-id'])
+                    jogador_2 = JogadorTorneio.objects.get(id_no_torneio=match['player2-id'])
+                    
+                    score_1 = int(match['scores-csv'][: match['scores-csv'][1:].find('-') + 1])
+                    score_2 = int(match['scores-csv'][match['scores-csv'][1:].find('-') + 2 :])
+                    
+                    partida = Partida(ganhador=ganhador, jogador_1=jogador_1, jogador_2=jogador_2, score_1=score_1,
+                                      score_2=score_2, round=round_torneio)
+                    partida.save()
+                    if score_1 == score_2 or score_1 < 0 or score_2 < 0:
+                        vitoria_por_ausencia = VitoriaAusencia(partida=partida)
+                        vitoria_por_ausencia.save()
+                
+                print(torneio, torneio.data)
+                for jogador in JogadorTorneio.objects.filter(torneio=torneio):
+                    if jogador.time:
+                        print(f'{jogador.time} | {jogador.nome}')
+                    else:
+                        print(jogador.nome)
+                    
+                for partida in Partida.objects.filter(jogador_1__torneio=torneio):
+                    print(f'{partida.jogador_1} {partida.score_1} vs {partida.score_2} {partida.jogador_2} -> {partida.ganhador} {partida.vitoria_por_ausencia()}')
+                
+                raise ValueError('TESTE')
+        except:
+            raise
         
-        # Retrieve a tournament by its id (or its url).
-        tournament = challonge.tournaments.show('atevalhalla2')
-        
-        # Tournaments, matches, and participants are all represented as normal Python dicts.
-        print(tournament['id'])
-        print(tournament['name']) 
-        print(tournament['started-at'])
-        
-        # Retrieve the participants for a given tournament.
-        participants = challonge.participants.index(tournament['id'])
-        print(len(participants))
-        for participant in participants:
-            print(participant['name'], participant['final-rank'])
-
-        matches = challonge.matches.index(tournament['id'])
-        print(len(matches))
-        for match in matches:
-            jogador_1 = jogador_por_id(participants, match['player1-id'])['name']
-            jogador_2 = jogador_por_id(participants, match['player2-id'])['name']
-            print(f'{jogador_1} {match["scores-csv"]} {jogador_2}')
             
 def jogador_por_id(jogadores, jogador_id):
     for jogador in jogadores:
---
+++ torneios/models.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Modelos usados para guardar torneios"""
+from django.db import models
+        
+class Torneio(models.Model):
+    data = models.DateField('Data do torneio')
+    nome = models.CharField('Nome do torneio', max_length=100)
+    url = models.URLField('URL do torneio')
+    adicionado_por = models.ForeignKey('jogadores.Jogador', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nome
+    
+class Round(models.Model):
+    indice = models.SmallIntegerField('Índice do round')
+    nome = models.CharField('Nome do round', max_length=20, default='')
+    torneio = models.ForeignKey('Torneio', on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('indice', 'torneio')
+        
+    def __str__(self):
+        if self.nome:
+            return self.nome
+        return self.indice
+    
+class Partida(models.Model):
+    jogador_1 = models.ForeignKey('JogadorTorneio', on_delete=models.CASCADE, related_name='jogador_1')
+    jogador_2 = models.ForeignKey('JogadorTorneio', on_delete=models.CASCADE, related_name='jogador_2')
+    score_1 = models.SmallIntegerField('Resultado do jogador 1')
+    score_2 = models.SmallIntegerField('Resultado do jogador 2')
+    round = models.ForeignKey('Round', on_delete=models.CASCADE)
+    ganhador = models.ForeignKey('JogadorTorneio', on_delete=models.CASCADE, related_name='ganhador')
+    
+    def vitoria_por_ausencia(self):
+        return hasattr(self, 'vitoriaausencia')
+    
+class VitoriaAusencia(models.Model):
+    """Define se partida foi vencida por ausência"""
+    partida = models.OneToOneField('Partida', on_delete=models.CASCADE)
+    
+class JogadorTorneio(models.Model):
+    """Vincula jogador a um torneio"""
+    nome = models.CharField('Nome do jogador', max_length=30)
+    time = models.ForeignKey('Time', on_delete=models.CASCADE, blank=True, null=True)
+    jogador = models.ForeignKey('jogadores.Jogador', on_delete=models.CASCADE, blank=True, null=True)
+    torneio = models.ForeignKey('Torneio', on_delete=models.CASCADE)
+    posicao_final = models.SmallIntegerField('Resultado no torneio')
+    valido = models.BooleanField('Jogador válido?', default=True)
+    id_no_torneio = models.IntegerField('Código de identificação no torneio')
+    
+    class Meta:
+        unique_together = (('nome', 'torneio', 'time'), ('jogador', 'torneio'))
+        
+    def __str__(self):
+        return self.nome
+        
+class Time(models.Model):
+    """Time a qual um jogador pertence em um torneio"""
+    nome = models.CharField('Nome do time', max_length=20)
+    
+    class Meta:
+        unique_together = ('nome',)
+        
+    def __str__(self):
+        return self.nome
