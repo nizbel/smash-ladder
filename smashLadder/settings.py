232fe70af8247e27c44fea949f3b4092df5cbcc5
nizbel <kingbowserii@gmail.com>
2019-09-06 18:44:49 -0300

    Corrigindo de vez agora...

--- smashLadder/settings.py
+++ smashLadder/settings.py
@@ -159,9 +159,9 @@ LOGOUT_REDIRECT_URL = 'inicio'
 NOME_SITE = 'CdL UnB'
 
 # Acelerar testes
- PASSWORD_HASHERS = [
-     'django.contrib.auth.hashers.MD5PasswordHasher',
- ]
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
 
 # Configuracoes extras
 from conf.conf import *
