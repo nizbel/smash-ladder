07a16f35ad7459c710a277a442b471860e6d1a48
nizbel <kingbowserii@gmail.com>
2019-10-20 16:24:54 -0300

    Removendo jogador de performance requisicao

--- performance/models.py
+++ performance/models.py
@@ -8,6 +8,5 @@ from smashLadder.utils import DateTimeFieldTz
 class PerformanceRequisicao(models.Model):
     data_hora_requisicao = DateTimeFieldTz('Data/hora da requisição')
     data_hora_resposta = DateTimeFieldTz('Data/hora da resposta')
-    jogador = models.ForeignKey('jogadores.Jogador', blank=True, null=True, on_delete=models.CASCADE)
     url = models.CharField('URL', max_length=255)
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
