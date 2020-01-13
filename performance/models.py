4561e0001cdfb2b11f3087a6ee10e5bb552c200f
nizbel <kingbowserii@gmail.com>
2020-01-13 01:46:58 -0300

    Corrigindo erro envolvendo apagar usuario

--- performance/models.py
+++ performance/models.py
@@ -9,4 +9,4 @@ class PerformanceRequisicao(models.Model):
     data_hora_requisicao = DateTimeFieldTz('Data/hora da requisição')
     data_hora_resposta = DateTimeFieldTz('Data/hora da resposta')
     url = models.CharField('URL', max_length=255)
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
